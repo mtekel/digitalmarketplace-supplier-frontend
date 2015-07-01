67b837b03f14103b9e238b838ac00298649fb824
Michal Tekel <mtekel@gmail.com>
2015-07-01 17:29:28 +0100

    add tsuru deploy script

---
+++ scripts/tsuru-deploy.sh
@@ -0,0 +1,12 @@
+tsuru app-create digitalmarketplace-supplier-frontend python
+tsuru env-set \
+DM_ADMIN_FRONTEND_COOKIE_SECRET=secret \
+DM_ADMIN_FRONTEND_PASSWORD_HASH=JHA1azIkMjcxMCRiNWZmMjhmMmExYTM0OGMyYTY0MjA3ZWFkOTIwNGM3NiQ4OGRLTHBUTWJQUE95UEVvSmg3djZYY2tWQ3lpcTZtaw== \
+DM_DATA_API_AUTH_TOKEN=ourtoken \
+DM_DATA_API_URL=https://digitalmarketplace-api-ci.tsuru2.paas.alphagov.co.uk \
+DM_MANDRILL_API_KEY=somekey \
+DM_PASSWORD_SECRET_KEY=verySecretKey \
+DM_S3_DOCUMENT_BUCKET=admin-frontend-dev-documents \
+DM_SEARCH_API_AUTH_TOKEN=CHbDLQtMvKoAuAtT8GM6vrdGGC \
+DM_SEARCH_API_URL=https://preview-search-api.development.digitalmarketplace.service.gov.uk
+tsuru app-deploy *
